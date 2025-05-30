bd33f1854b55b91882c484ee33cf2f180ac26acb
Natalia Taira <nataliagtaira@gmail.com>
2025-05-30 17:33:35 -0300

    Finaliza exercício 2

---
+++ aula04/ex02.ts
@@ -0,0 +1,17 @@
+import readLine from 'readline-sync';
+
+let wasFound = false;
+const numbers: Array<number> = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
+const numberToFind = readLine.questionInt('Digite o número que você deseja encontrar: ');
+
+
+for (let number of numbers) {
+    if (number === numberToFind) {
+        console.log(`\nO número ${numberToFind} está localizado na posição: ${numbers.indexOf(numberToFind)}`);
+        wasFound = true;
+    }
+}
+
+if (!wasFound) {
+    console.log(`\nO número ${numberToFind} não foi encontrado!`);
+}
