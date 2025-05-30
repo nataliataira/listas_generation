a278ea6944554b0ff17e2d267e261b1f01e9642c
Natalia Taira <nataliagtaira@gmail.com>
2025-05-30 15:29:32 -0300

    Finaliza exercicio 1

---
+++ aula04/ex01.ts
@@ -0,0 +1,20 @@
+import readLine from 'readline-sync';
+
+const colors: Array<string> = Array();
+const colorQuantity = 5;
+
+for (let index = 0; index < colorQuantity; index++) {
+    colors.push(readLine.question('Digite uma cor: '));
+}
+
+console.log('\nListar todas as cores: ');
+for (let color of colors) {
+    console.log(`${color}`);
+}
+
+colors.sort();
+
+console.log('\nOrdenar as cores: ');
+for (let color of colors) {
+    console.log(`${color}`);
+}
