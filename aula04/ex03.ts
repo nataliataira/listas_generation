b98c64d6b3d73079ac02ebbe50d3d4a364a5bc7e
Natalia Taira <nataliagtaira@gmail.com>
2025-05-30 17:49:51 -0300

    Remove comentários

--- aula04/ex03.ts
+++ aula04/ex03.ts
@@ -1,13 +1,5 @@
 import readLine from 'readline-sync';
 
-/**
- * Escreva um programa para criar uma Collection Set do tipo number. 
- * O programa deverá solicitar ao usuário, que ele digite via teclado 10 
- * valores inteiros não repetidos e adicione-os individualmente na Collection Set. 
- * Em seguida, faça o que se pede:
- *      Mostre na tela todos os elementos da Collection Set. 
- */
-
 let inputNumber = 0;
 let numberQuantity = 10;
 const numbers: Set<number> = new Set();
